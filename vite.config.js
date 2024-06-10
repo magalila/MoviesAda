50500832968ec332bb926706b35eff139f040af0
magalila <antonellamagalifigueredo@gmail.com>
2024-06-10 12:17:57 -0300

    correcciones de index.html

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: '../build', // Especifica el directorio de salida para los archivos generados
-    assetsDir: 'images', // Especifica el directorio de salida para los archivos estáticos (como imágenes)
-    manifest: true, // Genera un archivo manifest.json para la caché de archivos
-    rollupOptions: {
-      input: './public/index.html' // Especifica la ruta correcta hacia el archivo index.html
-    }
-  }
 })
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: '../build', // Especifica el directorio de salida para los archivos generados
+//     assetsDir: 'images', // Especifica el directorio de salida para los archivos estáticos (como imágenes)
+//     manifest: true, // Genera un archivo manifest.json para la caché de archivos
+//     rollupOptions: {
+//       input: './public/index.html' // Especifica la ruta correcta hacia el archivo index.html
+//     }
+//   }
+// })
