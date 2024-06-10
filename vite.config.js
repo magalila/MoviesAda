c5e694eb86be96264b650edbe5595317fdeeb215
magalila <antonellamagalifigueredo@gmail.com>
2024-06-10 13:53:06 -0300

    correcciones de index.html

--- vite.config.js
+++ vite.config.js
@@ -7,18 +7,3 @@ export default defineConfig({
 })
 
 
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     outDir: '../build', // Especifica el directorio de salida para los archivos generados
-//     assetsDir: 'images', // Especifica el directorio de salida para los archivos estáticos (como imágenes)
-//     manifest: true, // Genera un archivo manifest.json para la caché de archivos
-//     rollupOptions: {
-//       input: './public/index.html' // Especifica la ruta correcta hacia el archivo index.html
-//     }
-//   }
-// })
